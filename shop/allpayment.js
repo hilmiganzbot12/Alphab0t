1c438a44dcf44a182805fef8f90f383d359ada3d
hilmiganzbot12 <96254454+hilmiganzbot12@users.noreply.github.com>
2022-03-16 14:41:17 +0700

    Update allpayment.js

--- shop/allpayment.js
+++ shop/allpayment.js
@@ -1,27 +1,7 @@
 const allpayment = (prefix) => {
-return `PAYMENT BY ZEEONE OFC
+return `PAYMENT BY Black BotZ Official
+*Untuk pembayaran silahkan ketik .donasi dan scan jika suda melakukan pembayaran hubungi Owner Black BotZ Official
 
-*🏧 BANK*
-
-1. BRI 
-	ㅁ 8881 0887 4350 47326
-2. BCA 
-	ㅁ 3901 0887 4350 47326
-3. BNI 
-	ㅁ 881 0887 4350 47326
-4. PERMATA
-	ㅁ 852 8887 4350 47326
-	
-*💰 E-MONEY*
-
-1. GOPAY
-	ㅁ 0887 4350 47326
-2. OVO
-	ㅁ 0887 4350 47326
-3. DANA
-	ㅁ 0887 4350 47326
-	
-Semuanya atas nama RI****DI
 
 *📲 PULSA*
 
